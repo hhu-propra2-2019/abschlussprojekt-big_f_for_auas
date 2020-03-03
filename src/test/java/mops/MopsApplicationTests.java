1f3c22a6ff66198fc4ff529e4cf377422225cd2b
nichennig <vinzent.hennig@gmail.com>
2020-03-03 09:54:44 +0100

    spring init

---
+++ src/test/java/mops/MopsApplicationTests.java
@@ -0,0 +1,13 @@
+package mops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
