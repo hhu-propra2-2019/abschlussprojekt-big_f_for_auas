730e24b91bdc21f044c1f927ebbb7a2bffe2eb8c
GitStudentKanomble <lukas.becker@hhu.de>
2020-03-05 17:17:16 +0100

    Hinzufuegen einer Java Documentation fuer den DatePollCreateService.

--- src/main/java/mops/applicationServices/DatePollCreateService.java
+++ src/main/java/mops/applicationServices/DatePollCreateService.java
@@ -6,50 +6,108 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Dieser Service dient zur Erstellung einer Terminfindung (DatePoll).
+ * Der Service erhaelt die Daten zur Termindung durch den User (creator).
+ * Die Terminfingung wird dann durch einen Lombok-Builder erzeugt. Dieser durchlaueft mehrere Methoden.
+ *
+ * @author Niclas, Tim, Lukas
+ *
+ */
 @Service
 public class DatePollCreateService {
-    @SuppressWarnings("checkstyle:MissingJavadocMethod")
+    /**
+     *Initiale Methode zur Erstellung der Terminfindung.
+     * @param creator User der die Terminfindung erstellt.
+     * @return Lombok-Builder DatePoll Objekt.
+     */
     public DatePollBuilder initializeDatePoll(final User creator) {
         DatePoll datePoll = new DatePoll();
-        DatePollBuilder datePollBuilder = datePoll.builder();
+        DatePollBuilder datePollBuilder = DatePoll.builder();
         DatePollID datePollID = new DatePollID();
         datePollBuilder.datePollID(datePollID);
         datePollBuilder.creator(creator);
         return datePollBuilder;
     }
 
-    @SuppressWarnings("checkstyle:MissingJavadocMethod")
+    /**
+     * Zweite Instanz zur Erstellung der Terminfindung.
+     * Hinzufuegen der Meta-Informationen: Titel, Ort, Beschreibung.
+     *
+     * @param datePollBuilder Das Lombok-Builder Objekt aus initializeDatePoll.
+     * @param datePollMetaInf Meta-Informationen: Titel, Ort, Beschreibund.
+     * @return Lombok-Builder DatePoll Objekt.
+     */
     public DatePollBuilder addDatePollMetaInf(final DatePollBuilder datePollBuilder, final DatePollMetaInf datePollMetaInf) {
         datePollBuilder.datePollMetaInf(new DatePollMetaInf()); //Title Location Description
         return datePollBuilder;
     }
 
+    /**
+     * Dritte Instanz zur Erstellung der Terminfindung.
+     * Handelt es sich um eine offene (true) oder geschlossene (false) Terminfindung?
+     * @param datePollBuilder Das Lombok-Builder Objekt aus initializeDatePoll.
+     * @param openDatePoll offen (true) | geschlossen (false)
+     * @return Lombok-Builder DatePoll Objekt.
+     */
     public DatePollBuilder openOrClosedPoll(final DatePollBuilder datePollBuilder, final boolean openDatePoll) {
         datePollBuilder.usersCanCreateOption(openDatePoll);
         return datePollBuilder;
     }
 
-    @SuppressWarnings("checkstyle:WhitespaceAround")
+    /**
+     * Vierte Instanz zur Erstellung der Terminfindung.
+     * Liste der Datum-Eintraege an denen der Termin stattfinden kann.
+     * @param datePollBuilder Das Lombok-Builder Objekt aus initializeDatePoll.
+     * @param datePollOptions Alle Datums-Eintraege des User creator.
+     * @return Lombok-Builder DatePoll Objekt.
+     */
     public DatePollBuilder initDatePollOptionList(final DatePollBuilder datePollBuilder, final List<DatePollOption> datePollOptions) {
         datePollBuilder.datePollOptionList(datePollOptions);
         return datePollBuilder;
     }
 
+    /**
+     * Fuenfte Instanz zur Erstellung der Terminfindung.
+     * Angabe ob es sich um ein "single" oder "multiple" Choice Terminfindungs-Objekt handelt.
+     * @param datePollBuilder Das Lombok-Builder Objekt aus initializeDatePoll.
+     * @param singleChoicePoll Hat der User nur eine Wahlmoeglichkeit (true) oder mehrere (false).
+     * @return Lombok-Builder DatePoll Objekt.
+     */
     public DatePollBuilder setDatePollChoiceType(final DatePollBuilder datePollBuilder, final boolean singleChoicePoll) {
         datePollBuilder.singleChoiceDatePoll(singleChoicePoll);
         return datePollBuilder;
     }
 
+    /**
+     * Fuenfte Instanz zur Erstellung der Terminfindung.
+     * Angabe ob es priorisierte Auswahlmoeglichkeiten geben soll.
+     * @param datePollBuilder Das Lombok-Builder Objekt aus initializeDatePoll.
+     * @param priority Prioritaet an (true) oder aus (false).
+     * @return Lombok-Builder DatePoll Objekt.
+     */
     public DatePollBuilder setDatePollChoicePriority(final DatePollBuilder datePollBuilder, final boolean priority) {
         datePollBuilder.priorityChoice(priority);
         return datePollBuilder;
     }
 
+    /**
+     * Sechste Instanz zur Erstellung der Terminfindung.
+     * Angabe ob die Terminfindung anonym (true) oder öffentlich (false) stattfinden soll.
+     * @param datePollBuilder Das Lombok-Builder Objekt aus initializeDatePoll.
+     * @param isAnonymous Anonym (true) oder öffentlich (false).
+     * @return Lombok-Builder DatePoll Objekt.
+     */
     public DatePollBuilder setDatePollVisibilty(final DatePollBuilder datePollBuilder, final boolean isAnonymous) {
         datePollBuilder.datePollIsAnonymous(isAnonymous);
         return datePollBuilder;
     }
 
+    /**
+     * Die Methode gibt das fertige DatePoll Objekt zurueck.
+     * @param datePollBuilder Das Lombok-Builder Objekt aus initializeDatePoll.
+     * @return Das fertige DatePoll Objekt.
+     */
     public DatePoll buildDatePoll(final DatePollBuilder datePollBuilder) {
         return datePollBuilder.build();
     }
