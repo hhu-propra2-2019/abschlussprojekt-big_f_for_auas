163383f7ca6ae2db449eebd276e9d09d0d9fba07
Paul B <pabir101@hhu.de>
2020-03-09 11:15:53 +0100

    Spring Security und Webflow konfiguriert
    Checkstyle-Fehler müssen noch korrigiert werden

--- src/main/java/mops/TemplateTestController.java
+++ src/main/java/mops/TemplateTestController.java
@@ -3,10 +3,13 @@ package mops;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.annotation.security.RolesAllowed;
+
 // TODO: add javadoc
 @Controller
-public final class TemplateTestController {
+public class TemplateTestController {
 
+    @RolesAllowed({"ROLE_orga", "ROLE_studentin"})
     @GetMapping("/")
     public String returnDashboard() {
         return "mobile-dashboard";
