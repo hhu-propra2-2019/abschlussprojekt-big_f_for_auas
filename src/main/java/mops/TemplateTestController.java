3533195701116703c5c99caef1ebb2738f035e08
Henrik222111 <henrikhinzmann@web.de>
2020-03-09 09:07:09 +0100

    TestController aktualisiert

--- src/main/java/mops/TemplateTestController.java
+++ src/main/java/mops/TemplateTestController.java
@@ -11,34 +11,4 @@ public final class TemplateTestController {
     public String returnDashboard() {
         return "mobile-dashboard";
     }
-
-    @GetMapping("/wizard1")
-    public String returnWizard1() {
-        return "webflows/scheduling/mobileSchedulingNameSettings.html";
-    }
-
-    @GetMapping("/wizard2")
-    public String returnWizard2() {
-        return "webflows/scheduling/mobileSchedulingAccessSettings.html";
-    }
-
-    @GetMapping("/wizard3")
-    public String returnWizard3() {
-        return "webflows/scheduling/mobileSchedulingChoiceSettings.html";
-    }
-
-    @GetMapping("/wizard4")
-    public String returnWizard4() {
-        return "webflows/scheduling/mobileSchedulingChoiceOptions.html";
-    }
-
-    @GetMapping("/wizard5")
-    public String returnWizard5() {
-        return "webflows/scheduling/mobileSchedulingPublicationSettings.html";
-    }
-
-    @GetMapping("/wizard6")
-    public String returnWizard6() {
-        return "webflows/scheduling/mobileSchedulingConfirmation.html";
-    }
 }
