639750e60a2386c63fd0508eaba17c45dfaa9910
Daniel Curkovic <daniel.curkovic@hhu.de>
2020-03-10 08:50:12 +0100

    added Konstruktor of QuestionPoll

--- src/main/java/mops/domain/models/QuestionPoll/QuestionPoll.java
+++ src/main/java/mops/domain/models/QuestionPoll/QuestionPoll.java
@@ -1,10 +1,13 @@
 package mops.domain.models.QuestionPoll;
 
 import java.util.List;
-import mops.domain.models.User.User;
+
+import io.micrometer.shaded.org.pcollections.PBag;
+import mops.domain.models.User.UserId;
+
 
 public class QuestionPoll {
-  private User owner;
+  private UserId ownerId;
   private QuestionPollId questionPollId;
 
   private final QuestionPollHeader header;
@@ -13,4 +16,22 @@ public class QuestionPoll {
   private List<QuestionPollEntry> entries;
 
   private final QuestionPollBallot ballot;
+
+  public QuestionPoll(UserId pOwnerId,
+                      QuestionPollHeader pHeader,
+                      QuestionPollConfig pConfig,
+                      QuestionPollAccessibility pAccessor,
+                      List<QuestionPollEntry> pEntries,
+                      QuestionPollBallot pBallot) {
+
+    this.questionPollId = new QuestionPollId();
+    this.ownerId = pOwnerId;
+    this.header = pHeader;
+    this.config = pConfig;
+    this.accessor = pAccessor;
+    this.entries = pEntries;
+    this.ballot = pBallot;
+
+  }
+
 }
