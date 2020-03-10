46038bbc878c54f084e0b2a3ec53723b33201eac
GitStudentKanomble <lukas.becker@hhu.de>
2020-03-10 10:42:10 +0100

    Refactoring of checkstyle-suppressions.xml

--- src/main/java/mops/domain/models/datepoll/DatePollLifeCycle.java
+++ src/main/java/mops/domain/models/datepoll/DatePollLifeCycle.java
@@ -6,8 +6,14 @@ class DatePollLifeCycle {
     private Date startDate;
     private Date endDate;
 
+    /**
+     * Validierung der Datumsangaben im Konstruktor.
+     * @param startDate
+     * @param endDate
+     */
     DatePollLifeCycle(Date startDate, Date endDate) {
-        if(startDate.compareTo(endDate) < 0){
+        //startDate occurs before endDate
+        if (startDate.compareTo(endDate) < 0) {
             this.startDate = startDate;
             this.endDate = endDate;
         }
