15afa728a0924a9b5de623a0869b08a3e2996a0c
InsufferableStudent <50403256+InsufferableStudent@users.noreply.github.com>
2020-03-11 05:55:24 +0100

    added application Service javaDocs

--- src/main/java/mops/applicationService/QuestionPollCreationService.java
+++ src/main/java/mops/applicationService/QuestionPollCreationService.java
@@ -19,6 +19,7 @@ public final class QuestionPollCreationService {
 
   /**
    * Erstellt eine Factory für eine QuestionPoll und gibt diese zurück.
+   * Der übergebene Nutzer wird automatisch als Ersteller der QuestionPoll gesetzt.
    * @param userId
    * @return QuestionPollFactory
    */
@@ -27,22 +28,48 @@ public final class QuestionPollCreationService {
     return new QuestionPollFactory(userId);
   }
 
+  /** 
+   * Setzt den Header (Titel, Frage und optionale Beschreibung) in der Factory
+   * @param factory
+   * @param headerDto
+   */
   public void addHeader(final QuestionPollFactory factory, final QuestionPollHeaderDto headerDto) {
     factory.header(headerDto);
   }
 
+  /**
+   * Setzt die Entries (Elemente über die in der Abstimmung gewählt werden) in der Factory.
+   * @param factory
+   * @param entryDtoList
+   */
   public void addEntries(final QuestionPollFactory factory, final List<QuestionPollEntryDto> entryDtoList) {
     factory.entries(entryDtoList);
   }
 
+  /**
+   * Setzt die Konfiguration in der Factory. Die Konfiguration bestimmt ob die Abstimmung anonym ablaufen wird
+   * und ob es sich um eine Single-Choice Abstimmung oder eine Multi-Choice Abstimmung handelt. Die Konfiguration ist unveränderlich.
+   * @param factory
+   * @param configDto
+   */
   public void addConfig(final QuestionPollFactory factory, final QuestionPollConfigDto configDto) {
     factory.config(configDto);
   }
 
+  /**
+   * Setzt das Ballot Objekt in der Factory. Das Ballot aggregiert welche User für welche Option(en) abgestimmt haben.
+   * @param factory
+   * @param ballotDto
+   */
   public void addBallot(final QuestionPollFactory factory, final QuestionPollBallotDto ballotDto) {
     factory.ballot(ballotDto);
   }
 
+  /**
+   * Setzt das Lifecycle Objekt in der Factory. Das Lifecycleobjekt bestimmt in welchem Zeitraum abgestimmt werden kann.
+   * @param factory
+   * @param lifecycleDto
+   */
   public void addLifecycle(final QuestionPollFactory factory, final QuestionPollLifecycleDto lifecycleDto) {
     factory.lifecycle(lifecycleDto);
   }
--- src/main/java/mops/applicationService/QuestionPollSyndicationService.java
+++ src/main/java/mops/applicationService/QuestionPollSyndicationService.java
@@ -13,13 +13,23 @@ public class QuestionPollSyndicationService {
 
     private QuestionPollRepositoryInterface questionPollRepo;
 
-    public QuestionPollLink generateLink(final QuestionPollFactory factory) {
+    /** Schließt den Factory Prozess ab und speichert den generierte QuestionPolll ab.
+     * Gibt generierte Url zurück
+     * @param factory
+     * @return QuestionPollLink
+     */
+    public QuestionPollLink publishQuestionPoll(final QuestionPollFactory factory) {
         QuestionPollId questionPollId = questionPollRepo.save(factory.build());
         QuestionPollLink link = questionPollRepo.getUrl(questionPollId);
         return link;
     }
 
-    public void addUserId(final QuestionPollFactory factory , UserId... userId) {
+    /** Fügt einen weiteren Nutzer zu der Liste der Nutzer hinzu, die an einer Umfrage Teilnehmen dürfen.
+     * Darf nur aufgerufen werden, wenn es sich um eine geschlossene Umfrage handelt (restrictedAccess = true)
+     * @param factory
+     * @param userId
+     */
+    public void addParticipator(final QuestionPollFactory factory, UserId... userId) {
         factory.accessibilityAddUser(userId);
     }
 }
