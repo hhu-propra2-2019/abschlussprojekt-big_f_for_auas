df805842b6a7fd0ce5cf467f0cae64010d44ef31
Dominik Brandt <dominik@drei01.de>
2020-03-11 10:47:19 +0100

    Maximale Start/End-Zeit ist 100 Jahre ab Erstellungstag

--- src/main/resources/static/accessSettings.js
+++ src/main/resources/static/accessSettings.js
@@ -8,7 +8,9 @@ function initDatepicker(){
     document.getElementById("termin-startDate").value = day;
     document.getElementById("termin-startTime").value = "12:00";
     document.getElementById("termin-startDate").min = day;
+    document.getElementById("termin-startDate").max = addMaxYear(today);
     document.getElementById("termin-endDate").min = day;
+    document.getElementById("termin-endDate").max = addMaxYear(today);
     document.getElementById("termin-endTime").value = "12:00";
     document.getElementById("termin-endDate").value = addOneMonth(today);
 }
@@ -16,5 +18,11 @@ function initDatepicker(){
 function addOneMonth(dateString){
     let date = new Date(dateString);
     date.setMonth(date.getMonth()+1);
+    return date.toISOString().split("T")[0];
+}
+
+function addMaxYear(dateString){
+    let date = new Date(dateString);
+    date.setFullYear(date.getFullYear()+100);
     return date.toISOString().split("T")[0];
 }
