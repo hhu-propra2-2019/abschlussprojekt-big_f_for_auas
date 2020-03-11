219df416bca9d70e8e1db73f0e4325d5c1ee50c2
InsufferableStudent <50403256+InsufferableStudent@users.noreply.github.com>
2020-03-11 09:19:07 +0100

    added javadoc for QuestionPollConfig

--- src/main/java/mops/domain/models/QuestionPoll/QuestionPollConfig.java
+++ src/main/java/mops/domain/models/QuestionPoll/QuestionPollConfig.java
@@ -2,6 +2,10 @@ package mops.domain.models.QuestionPoll;
 
 import lombok.Value;
 
+/**
+ * Speichert Einstellungen ob die QuestionPoll anonym ist (usingAlias = true) und ob es sich um eine
+ *  Multi-Choice Umfrage geben.
+ */
 @Value
 public class QuestionPollConfig {
 
