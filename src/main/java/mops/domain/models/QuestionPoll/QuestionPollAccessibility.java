1a367461966967caad5ec3fe5184f3c8f9c33a00
InsufferableStudent <50403256+InsufferableStudent@users.noreply.github.com>
2020-03-11 06:03:39 +0100

    added Accesbility javaDoc

--- src/main/java/mops/domain/models/QuestionPoll/QuestionPollAccessibility.java
+++ src/main/java/mops/domain/models/QuestionPoll/QuestionPollAccessibility.java
@@ -4,9 +4,16 @@ import java.util.Set;
 import lombok.Value;
 import mops.domain.models.User.UserId;
 
+/**
+ * Value Objekt welches die Konfiguration über den Zugriff auf die Abstimmung abkapselt.
+ * Ist restrictedAccess = true, handelt es sich um eine Abstimmung in der es nur einer begrenzten Anzahl
+ * von Nutzern erlaubt ist abzustimmen. Die Nutzer werden in diesem Fall in der participants Liste gepseichert.
+ * Ist restrictedAccess = false, handelt es sich um eine offene Abstimmung und jeder kann abstimmen.
+ * In diesem Fall spielt die participants Liste keine weitere Rolle.
+ */
 @Value
 public class QuestionPollAccessibility {
 
-  final boolean restrictedAccess;
-  final Set<UserId> participants;
+  private final boolean restrictedAccess;
+  private final Set<UserId> participants;
 }
