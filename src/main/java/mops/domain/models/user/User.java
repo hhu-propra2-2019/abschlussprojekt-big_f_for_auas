7de4b9b9b6b53cf52e981524e3a9958228082c74
InsufferableStudent <50403256+InsufferableStudent@users.noreply.github.com>
2020-03-12 10:03:01 +0100

    added getter import

--- src/main/java/mops/domain/models/user/User.java
+++ src/main/java/mops/domain/models/user/User.java
@@ -2,9 +2,11 @@ package mops.domain.models.user;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 @AllArgsConstructor
 @EqualsAndHashCode
 public class User {
-    @Getter private final UserId id;
+    @Getter
+    private final UserId id;
 }
