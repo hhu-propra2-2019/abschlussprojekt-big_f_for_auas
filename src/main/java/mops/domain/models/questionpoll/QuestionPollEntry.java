743ea024094549d01c26dfd47dd76eb6c2dbd2bd
InsufferableStudent <50403256+InsufferableStudent@users.noreply.github.com>
2020-03-12 08:48:08 +0100

    QuestionPollEntry is no longer a value Objekt

--- src/main/java/mops/domain/models/questionpoll/QuestionPollEntry.java
+++ src/main/java/mops/domain/models/questionpoll/QuestionPollEntry.java
@@ -1,12 +1,14 @@
 package mops.domain.models.questionpoll;
 
+import lombok.AllArgsConstructor;
 import lombok.Value;
 
 /**
  * Speichert eine Option über die in einem QuestionPoll abgestimmt werden kann und
  *  verfolgt wie oft für diese Option abgestimmt wurde.
  */
-@Value
+
+@AllArgsConstructor
 public class QuestionPollEntry {
   private final String title;
   private long count;
