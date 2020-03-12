8cdc0e282ff0615c6aba6ad5e15766a7e1338f66
Dominik Brandt <dominik@drei01.de>
2020-03-12 12:20:39 +0100

    Mehr Frontend Validierungen hinzugefügt

--- src/main/resources/static/choiceOptions.js
+++ src/main/resources/static/choiceOptions.js
@@ -2,8 +2,8 @@ function addSchedulingChoiceOption(){
     $('#scheduleOptionList').append(`            <li>
                 <div class="form-group">
                     <div class="input-group">
-                        <input name="dateOption" class="form-control ml-4" type="date">
-                        <input class="form-control optionTime" type="time" value="12:00">
+                        <input name="dateOption" class="form-control ml-4" type="date" required>
+                        <input class="form-control optionTime" type="time" value="12:00" required>
                         <div class="input-group-append">
                             <button type="button" name="removeOption" class="btn btn-danger" onclick="removeChoiceOption(this)"><i class="fa fa-minus"></i></button>
                         </div>
@@ -16,7 +16,7 @@ function addPollChoiceOption(){
     $('#pollOptionList').append(`            <li>
                 <div class="form-group">
                     <div class="input-group">
-                        <input name="pollOption" class="form-control ml-4" type="text" placeholder="Möglichkeit eingeben">
+                        <input name="pollOption" class="form-control ml-4" type="text" placeholder="Möglichkeit eingeben" required>
                         <div class="input-group-append">
                             <button type="button" name="removeOption" class="btn btn-danger" onclick="removeChoiceOption(this)"><i class="fa fa-minus"></i></button>
                         </div>
