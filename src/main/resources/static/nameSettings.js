c0243c1a9749b420aa255a02e3702c28178a5e20
Dominik Brandt <dominik@drei01.de>
2020-03-12 09:57:00 +0100

    Validierungs JS ausgelagert

---
+++ src/main/resources/static/nameSettings.js
@@ -0,0 +1,14 @@
+'use strict';
+    window.addEventListener('load', function() {
+        let forms = document.getElementsByClassName('needs-validation');
+        let validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+
