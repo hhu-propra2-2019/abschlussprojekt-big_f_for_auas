8848d5d08b780fb9e4af064b656ab6d98f55e1f1
nichennig <vinzent.hennig@gmail.com>
2020-03-17 08:39:13 +0100

    removed FieldErrorNames

---
+++ src/main/java/mops/domain/models/FieldErrorNames.java
@@ -0,0 +1,18 @@
+package mops.domain.models;
+
+public enum FieldErrorNames {
+    DATE_POLL_IDENTIFIER(DatePollFields.DATE_POLL_LINK),
+    DATE_POLL_LOCATION(DatePollFields.DATE_POLL_META_INF),
+    DATE_POLL_LIFECYCLE(DatePollFields.DATE_POLL_META_INF);
+
+    private final DatePollFields parent;
+
+    FieldErrorNames(DatePollFields parent) {
+        this.parent = parent;
+    }
+
+    public boolean isChildOf(DatePollFields field) {
+        return this.parent == field;
+    }
+
+}
