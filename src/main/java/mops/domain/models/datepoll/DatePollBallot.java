eda143f47a0f1b818f1e0d89d885ca5ecc01bdc1
Daniel Curkovic <daniel.curkovic@hhu.de>
2020-03-18 12:34:17 +0100

    added DatePollBallot

---
+++ src/main/java/mops/domain/models/datepoll/DatePollBallot.java
@@ -0,0 +1,47 @@
+package mops.domain.models.datepoll;
+
+import mops.domain.models.ValidateAble;
+import mops.domain.models.Validation;
+import mops.domain.models.user.UserId;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+public class DatePollBallot implements ValidateAble {
+    private final UserId user;
+    private final Set<DatePollEntry> selectedEntriesYes;
+    private final Set<DatePollEntry> selectedEntriesMaybe;
+
+    /**
+     * Konstruktor wo man ja und vielleicht abstimmen kann.
+     * @param qpUserId
+     * @param selectedEntriesYes
+     * @param selectedEntriesMaybe
+     */
+    public DatePollBallot(UserId qpUserId, Set<DatePollEntry> selectedEntriesYes, Set<DatePollEntry> selectedEntriesMaybe) {
+        this.user = qpUserId;
+        this.selectedEntriesYes = Collections.unmodifiableSet(selectedEntriesYes);
+        this.selectedEntriesMaybe = Collections.unmodifiableSet(selectedEntriesMaybe);
+    }
+
+    /** Konstruktor wo man ja und vielleicht abstimmen kann.
+     *
+     * @param qpUserId
+     * @param selectedEntriesYes
+     */
+    public DatePollBallot(UserId qpUserId, Set<DatePollEntry> selectedEntriesYes) {
+        this.user = qpUserId;
+        this.selectedEntriesYes = Collections.unmodifiableSet(selectedEntriesYes);
+        this.selectedEntriesMaybe = Collections.unmodifiableSet(Collections.emptySet());
+    }
+    /**
+     * ...
+     * @return...
+     */
+    @Override
+    public Validation validate() {
+        return Validation.noErrors();
+    }
+}
