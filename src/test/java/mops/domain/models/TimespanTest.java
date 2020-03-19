e8927ecb810909982d9e0328d7cc968f78e68162
Henrik222111 <henrikhinzmann@web.de>
2020-03-19 09:48:14 +0100

    Build Successful

--- src/test/java/mops/domain/models/TimespanTest.java
+++ src/test/java/mops/domain/models/TimespanTest.java
@@ -7,8 +7,8 @@ import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TimespanTest {
-    private Validation testValidator;
+public class TimespanTest { //NOPMD
+    private Validation testValidator; //NOPMD
 
     /**
      * Erstellt leeren validator der in jedem Test angepasst wird.
@@ -19,52 +19,58 @@ public class TimespanTest {
     }
 
     @Test
+    @SuppressWarnings({"PMD.LawOfDemeter"}) //NOPMD
     public void nullStartTime() {
         final Timespan timespan = new Timespan(null, LocalDateTime.now());
         this.testValidator = this.testValidator.appendValidation(new Validation(FieldErrorNames.TIMESPAN_START_NULL));
-        Validation validator = timespan.validate();
+        final Validation validator = timespan.validate();
         assertThat(this.testValidator.getErrorMessages()).isEqualTo(validator.getErrorMessages());
     }
 
     @Test
+    @SuppressWarnings({"PMD.LawOfDemeter"})
     public void nullEndTime() {
         final Timespan timespan = new Timespan(LocalDateTime.now(), null);
         this.testValidator = this.testValidator.appendValidation(new Validation(FieldErrorNames.TIMESPAN_END_NULL));
-        Validation validator = timespan.validate();
+        final Validation validator = timespan.validate();
         assertThat(this.testValidator.getErrorMessages()).isEqualTo(validator.getErrorMessages());
     }
 
     @Test
+    @SuppressWarnings({"PMD.LawOfDemeter"})
     public void bothNull() {
         final Timespan timespan = new Timespan(null, null);
         this.testValidator = this.testValidator.appendValidation(new Validation(FieldErrorNames.TIMESPAN_START_NULL));
         this.testValidator = this.testValidator.appendValidation(new Validation(FieldErrorNames.TIMESPAN_END_NULL));
-        Validation validator = timespan.validate();
+        final Validation validator = timespan.validate();
         assertThat(this.testValidator.getErrorMessages()).isEqualTo(validator.getErrorMessages());
     }
 
     @Test
+    @SuppressWarnings({"PMD.LawOfDemeter"})
     public void swappedTime() {
         final Timespan timespan = new Timespan(LocalDateTime.now().plusMinutes(1), LocalDateTime.now());
         this.testValidator = this.testValidator.appendValidation(new Validation(FieldErrorNames.TIMESPAN_SWAPPED));
-        Validation validator = timespan.validate();
+        final Validation validator = timespan.validate();
         assertThat(this.testValidator.getErrorMessages()).isEqualTo(validator.getErrorMessages());
     }
 
     @Test
+    @SuppressWarnings({"PMD.LawOfDemeter"})
     public void sameTime() {
-        LocalDateTime time1 = LocalDateTime.now();
-        LocalDateTime time2 = time1;
+        final LocalDateTime time1 = LocalDateTime.now();
+        final LocalDateTime time2 = time1;
         final Timespan timespan = new Timespan(time1, time2);
         this.testValidator = this.testValidator.appendValidation(new Validation(FieldErrorNames.TIMESPAN_SAME));
-        Validation validator = timespan.validate();
+        final Validation validator = timespan.validate();
         assertThat(this.testValidator.getErrorMessages()).isEqualTo(validator.getErrorMessages());
     }
 
     @Test
+    @SuppressWarnings({"PMD.LawOfDemeter"})
     public void validTime() {
         final Timespan timespan = new Timespan(LocalDateTime.now(), LocalDateTime.now().plusMinutes(1));
-        Validation validator = timespan.validate();
+        final Validation validator = timespan.validate();
         assertThat(this.testValidator.getErrorMessages()).isEqualTo(validator.getErrorMessages());
     }
 }
