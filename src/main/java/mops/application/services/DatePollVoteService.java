c1718104ad74eb0ee42ef12e33986e2c4007a530
Daniel Curkovic <daniel.curkovic@hhu.de>
2020-03-20 10:13:39 +0100

    1. entwurf

--- src/main/java/mops/application/services/DatePollVoteService.java
+++ src/main/java/mops/application/services/DatePollVoteService.java
@@ -1,8 +1,18 @@
 package mops.application.services;
 
 
+import mops.controllers.dtos.DatePollEntryDto;
+import mops.domain.models.Timespan;
+import mops.domain.models.datepoll.DatePoll;
+import mops.domain.models.datepoll.DatePollEntry;
+import mops.domain.models.datepoll.DatePollLink;
+import mops.domain.models.user.UserId;
 import mops.domain.repositories.DatePollRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class DatePollVoteService {
@@ -14,4 +24,18 @@ public class DatePollVoteService {
         this.datePollRepository = datePollRepository;
     }
 
+    public void vote(DatePollLink link, UserId user, Set<DatePollEntryDto> yes, Set<DatePollEntryDto> maybe) {
+        Optional<DatePoll> datePoll = datePollRepository.load(link);
+        if(datePoll.isPresent()) {
+            datePoll.get().castBallot(user, convert(yes), convert(maybe));
+        } else {
+            // ?
+        }
+    }
+
+    private Set<DatePollEntry> convert(Set<DatePollEntryDto> dtos) {
+        return dtos.stream()
+                .map(dto -> new DatePollEntry(dto.getSuggestedPeriod()))
+                .collect(Collectors.toSet());
+    }
 }
