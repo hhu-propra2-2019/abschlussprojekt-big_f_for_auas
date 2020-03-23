e7fbfb658511fd7b9bf02ece610f21ff23d12962
Paul B <pabir101@hhu.de>
2020-03-23 18:02:57 +0100

    PMD, endlich

--- src/test/java/mops/database/UserVotesForDatePollTest.java
+++ src/test/java/mops/database/UserVotesForDatePollTest.java
@@ -61,8 +61,8 @@ public class UserVotesForDatePollTest {
         final DatePollConfig datePollConfig = new DatePollConfig();
         final PollLink datePollLink = new PollLink();
 
-        final Set<UserId> participants = new HashSet<>();
-        IntStream.range(0, 3).forEach(i -> participants.add(new UserId(Integer.toString(i))));
+        //final Set<UserId> participants = new HashSet<>();
+        //IntStream.range(0, 3).forEach(i -> participants.add(new UserId(Integer.toString(i))));
 
         final Set<DatePollEntry> pollEntries = new HashSet<>();
         IntStream.range(0, 3).forEach(i -> pollEntries.add(new DatePollEntry(
