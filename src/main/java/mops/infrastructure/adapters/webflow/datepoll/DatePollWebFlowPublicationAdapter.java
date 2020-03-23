c8979acc208059b3d22b3c5238182ad3016ae1b9
Paul B <pabir101@hhu.de>
2020-03-23 13:06:27 +0100

    Confirmation-Template angepasst

--- src/main/java/mops/infrastructure/adapters/webflow/datepoll/DatePollWebFlowPublicationAdapter.java
+++ src/main/java/mops/infrastructure/adapters/webflow/datepoll/DatePollWebFlowPublicationAdapter.java
@@ -1,32 +1,30 @@
 package mops.infrastructure.adapters.webflow.datepoll;
 
-import lombok.NoArgsConstructor;
+import mops.application.services.PublicationService;
 import mops.domain.models.datepoll.DatePoll;
 import mops.infrastructure.adapters.webflow.datepoll.dtos.ConfigDto;
 import mops.infrastructure.adapters.webflow.datepoll.dtos.ConfirmationDto;
 import mops.infrastructure.adapters.webflow.datepoll.dtos.EntriesDto;
 import mops.infrastructure.adapters.webflow.datepoll.dtos.MetaInfDto;
 import mops.infrastructure.adapters.webflow.dtos.PublicationDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@NoArgsConstructor
 public final class DatePollWebFlowPublicationAdapter {
 
-    /*private final transient PublicationService publicationService;
+    private final transient PublicationService publicationService;
 
     @Autowired
     public DatePollWebFlowPublicationAdapter(PublicationService publicationService) {
         this.publicationService = publicationService;
-    }*/
-
-    // TODO: Implementieren, offensichtlich
+    }
 
     public ConfirmationDto buildConfirmationDto(MetaInfDto metaInfDto,
                                                 ConfigDto configDto,
                                                 EntriesDto entriesDto,
                                                 PublicationDto publicationDto) {
-        return null;
+        return new ConfirmationDto(metaInfDto, configDto, entriesDto, publicationDto);
     }
 
     public DatePoll publishDatePoll(ConfirmationDto confirmationDto) {
--- src/main/java/mops/infrastructure/adapters/webflow/datepoll/dtos/ConfirmationDto.java
+++ src/main/java/mops/infrastructure/adapters/webflow/datepoll/dtos/ConfirmationDto.java
@@ -1,11 +1,22 @@
 package mops.infrastructure.adapters.webflow.datepoll.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import mops.infrastructure.adapters.webflow.dtos.GeneralDto;
+import mops.infrastructure.adapters.webflow.dtos.PublicationDto;
 
 import java.io.Serializable;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class ConfirmationDto implements Serializable {
-    // TODO: füllen
+@AllArgsConstructor
+public class ConfirmationDto extends GeneralDto implements Serializable {
+
     public static final long serialVersionUID = 786187492712436L;
+
+    private MetaInfDto metaInfDto;
+    private ConfigDto configDto;
+    private EntriesDto entriesDto;
+    private PublicationDto publicationDto;
 }
