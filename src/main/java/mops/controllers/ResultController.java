9764c19a674718111024ca9b6c66639445c0aae6
AdlasDougams <Tim.Knittel@yahoo.de>
2020-03-25 11:29:04 +0100

    QuestionPoll und Zugehöriges refactored

--- src/main/java/mops/controllers/ResultController.java
+++ src/main/java/mops/controllers/ResultController.java
@@ -1,5 +1,5 @@
 package mops.controllers;
-
+/*
 import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.SortedSet;
@@ -21,37 +21,7 @@ public class ResultController {
     private final transient DatePollInfoAdapter datePollInfoAdapter;
 
 
-    private SortedSet<DatePollResultDto> createFakeDatePollResultDtos()
-    {
-        DatePollResultDto fakeResultDto1 = new DatePollResultDto();
-        fakeResultDto1.setTimespan(new Timespan(LocalDateTime.now(), LocalDateTime.now().plusHours(2)));
-        fakeResultDto1.setYesVotes(14);
-        fakeResultDto1.setMaybeVotes(10);
 
-        DatePollResultDto fakeResultDto2 = new DatePollResultDto();
-        fakeResultDto2.setTimespan(new Timespan(LocalDateTime.now().plusDays(2),
-                LocalDateTime.now().plusDays(2).plusHours(2)));
-        fakeResultDto2.setYesVotes(10);
-        fakeResultDto2.setMaybeVotes(0);
-
-        DatePollResultDto fakeResultDto3 = new DatePollResultDto();
-        fakeResultDto3.setTimespan(new Timespan(LocalDateTime.now().minusDays(2), LocalDateTime.now().minusDays(2).plusHours(2)));
-        fakeResultDto3.setYesVotes(5);
-        fakeResultDto3.setMaybeVotes(15);
-
-        DatePollResultDto fakeResultDto4 = new DatePollResultDto();
-        fakeResultDto4.setTimespan(new Timespan(LocalDateTime.now().minusDays(1), LocalDateTime.now().minusDays(1).plusHours(2)));
-        fakeResultDto4.setYesVotes(17);
-        fakeResultDto4.setMaybeVotes(3);
-
-        SortedSet<DatePollResultDto> testData = new TreeSet<DatePollResultDto>();
-        testData.add(fakeResultDto1);
-        testData.add(fakeResultDto2);
-        testData.add(fakeResultDto3);
-        testData.add(fakeResultDto4);
-
-        return testData;
-    }
     @Autowired
     public ResultController(DatePollInfoAdapter datePollInfoAdapter) {
         this.datePollInfoAdapter = datePollInfoAdapter;
@@ -63,13 +33,12 @@ public class ResultController {
      * @param link
      * @return String.
      */
-    @GetMapping("/result/{link}")
+  /*  @GetMapping("/result/{link}")
     public String mapResults(Model model, @PathVariable String link) {
         //final SortedSet<DatePollResultDto> results = datePollInfoAdapter.getAllDatePollResultDto(new PollLink(link));
-        final SortedSet<DatePollResultDto> results = createFakeDatePollResultDtos();
         //final DatePollMetaInfDto metaInf = datePollInfoAdapter.showDatePollMetaInformation(new PollLink(link));
         model.addAttribute("results", results);
         //model.addAttribute("metaInf", metaInf);
         return "mobilePollResults";
     }
-}
+}*/
