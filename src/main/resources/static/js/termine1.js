cf70bf5c965c9366b232891f2a675ba01b50be18
Dominik Brandt <dobra101@uni-duesseldorf.de>
2020-03-27 14:29:49 +0100

    multiple choice als default

--- src/main/resources/static/js/termine1.js
+++ src/main/resources/static/js/termine1.js
@@ -1,3 +1,8 @@
+$(document).ready(function() {
+    $('#collapsePriorities').collapse('show');
+    });
+
+
 function toggleGroup(elem){
     $(elem).siblings('input[type="checkbox"]').not(this).prop('checked', false);
 }
