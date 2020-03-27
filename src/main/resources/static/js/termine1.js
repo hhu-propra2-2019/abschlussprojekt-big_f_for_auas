b3689a6aa04eb4e8996bc202300a779888567e9c
Dominik Brandt <dobra101@uni-duesseldorf.de>
2020-03-27 13:57:47 +0100

    ja/nein/vielleicht kann jetzt erst gewählt werden, nachdem Multiple Choice gewählt wurde

--- src/main/resources/static/js/termine1.js
+++ src/main/resources/static/js/termine1.js
@@ -3,19 +3,11 @@ function toggleGroup(elem){
 }
 
 function showPriorities(){
-    if( $("input[name=single-choice]:checked").val() === "false" ){
+    if( $("#multiple-choice:checked").val() === "false" ){
         $('#collapsePriorities').collapse('show');
     }
     else{
         $('#collapsePriorities').collapse('hide');
-    }
-}
-function showMutable(){
-    if( $("input[name=choice-mutable]").is(':checked') ){
-        $("input[name=choice-addNew]").prop('disabled', false)
-    }
-    else{
-        $("input[name=choice-addNew]").prop('checked', false).prop('disabled', true);
     }
 }
 
