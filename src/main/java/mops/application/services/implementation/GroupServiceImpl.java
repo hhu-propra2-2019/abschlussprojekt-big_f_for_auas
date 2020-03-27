daf148788a7b71dcd25c74f075cf19ab0a174788
nichennig <vinzent.hennig@gmail.com>
2020-03-27 09:47:01 +0100

    added a controll that the user is member of a group

--- src/main/java/mops/application/services/implementation/GroupServiceImpl.java
+++ src/main/java/mops/application/services/implementation/GroupServiceImpl.java
@@ -1,7 +1,9 @@
 package mops.application.services.implementation;
 
 import mops.application.services.GroupService;
+import mops.domain.models.group.Group;
 import mops.domain.models.group.GroupId;
+import mops.domain.models.user.UserId;
 import mops.infrastructure.rest.GroupRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,11 +20,27 @@ public class GroupServiceImpl implements GroupService {
 
     /**
      * Gibt zurück, ob eine Gruppe existiert.
+     *
      * @param groupId Id der Gruppe, die angefragt wird.
      * @return boolean, der angibt, ob die Gruppe existiert.
      */
     @Override
     public boolean groupExists(GroupId groupId) {
         return groupRepository.existsById(groupId);
+    }
+
+    /**
+     * Prüft ob der User mitglied in der Gruppe ist.
+     *
+     * @param userId
+     * @param groupId
+     * @return boolean, der angibt true wird wenn der Nutzer Mitglied ist.
+     */
+    @SuppressWarnings({"PMD.LawOfDemeter"})
+    @Override
+    public boolean isUserInGroup(UserId userId, GroupId groupId) {
+        final Group group = groupRepository.getGroupById(groupId);
+        return group.getUser().stream()
+                .anyMatch(id -> id.equals(userId));
     }
 }
